662eda2421c81d38decb23c195272650615104b9
BJulOs <julesbresson88600@gmail.com>
2023-05-05 10:55:40 +0200

    Question 1

--- Projet/src/gift.appli/public/index.php
+++ Projet/src/gift.appli/public/index.php
@@ -9,4 +9,20 @@ $app = (require_once __DIR__ . '/../src/conf/bootstrap.php')();
 /* routes loading */
 (require_once __DIR__ . '/../src/conf/routes.php')($app);
 
+\gift\app\services\utils\Eloquent::init(__DIR__ . '/../src/conf/gift.db.conf.ini');
+
+/**
+ * Question 1
+ */
+
+echo "Question 1 :" . PHP_EOL;
+foreach (\gift\app\models\Prestation::all() as $presta) {
+	echo $presta->libelle . PHP_EOL;
+	echo $presta->description . PHP_EOL;
+	echo $presta->tarif . PHP_EOL;
+	echo $presta->unit . PHP_EOL;
+	echo "-------------------" . PHP_EOL;
+}
+echo "Fin : Question 1" . PHP_EOL;
+
 $app->run();
