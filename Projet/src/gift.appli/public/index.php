6d516dc34fbbb5fea932fee1bd2f4c632c0a6a53
BJulOs <julesbresson88600@gmail.com>
2023-05-05 11:00:08 +0200

    Question 2 & 3

--- Projet/src/gift.appli/public/index.php
+++ Projet/src/gift.appli/public/index.php
@@ -15,6 +15,8 @@ $app = (require_once __DIR__ . '/../src/conf/bootstrap.php')();
  * Question 1
  */
 
+// Lister les prestations ; pour chaque prestation, afficher le libellé, la description, le tarif et l'unité.
+
 echo "Question 1 :" . PHP_EOL;
 foreach (\gift\app\models\Prestation::all() as $presta) {
 	echo $presta->libelle . PHP_EOL;
@@ -24,5 +26,41 @@ foreach (\gift\app\models\Prestation::all() as $presta) {
 	echo "-------------------" . PHP_EOL;
 }
 echo "Fin : Question 1" . PHP_EOL;
+
+/**
+ * Question 2
+ */
+
+// Lister les prestations ; pour chaque prestation, afficher le libellé, la description, le tarif et l'unité. Afficher
+// de plus la catégorie de la prestation. On utilisera un chargement lié (eager loading).
+
+echo "Question 2 :" . PHP_EOL;
+foreach (\gift\app\models\Prestation::with('category')->get() as $presta) {
+	echo $presta->libelle . PHP_EOL;
+	echo $presta->description . PHP_EOL;
+	echo $presta->tarif . PHP_EOL;
+	echo $presta->unit . PHP_EOL;
+	echo $presta->category->libelle . PHP_EOL;
+	echo "-------------------" . PHP_EOL;
+}
+echo "Fin : Question 2" . PHP_EOL;
+
+/**
+ * Question 3
+ */
+
+// Afficher la catégorie 3 (libellé) et la liste des prestations (libellé, tarif, unité) de cette catégorie.
+// On utilisera impérativement la méthode implantant l'association.
+
+echo "Question 3 :" . PHP_EOL;
+$category = \gift\app\models\Category::find(3);
+echo $category->libelle . PHP_EOL;
+foreach ($category->prestations as $presta) {
+	echo $presta->libelle . PHP_EOL;
+	echo $presta->tarif . PHP_EOL;
+	echo $presta->unit . PHP_EOL;
+	echo "-------------------" . PHP_EOL;
+}
+echo "Fin : Question 3" . PHP_EOL;
 
 $app->run();
