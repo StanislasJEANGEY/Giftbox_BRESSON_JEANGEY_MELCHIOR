60d4d8973b2aca24e3aeea3582e3b8f10ff11c8c
BJulOs <julesbresson88600@gmail.com>
2023-05-05 09:30:42 +0200

    Squelette

---
+++ Projet/src/gift.appli/src/models/Categorie.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace girft\app\models;
+
+class Categorie extends \Illuminate\Database\Eloquent\Model {
+
+	protected $table = 'categorie';
+	protected $primaryKey = 'id';
+	public $timestamps = false;
+
+	public function prestations() : \Illuminate\Database\Eloquent\Relations\HasMany {
+		return $this->hasMany(Prestation::class, 'cat_id');
+	}
+
+}
