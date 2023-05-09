e44287a43b810c8c2db99b966312bb0a2ae7d77b
Stanislas JEANGEY <jeangey1u@etu.univ-lorraine.fr>
2023-05-09 09:32:09 +0200

    hello world

--- Projet/src/gift.appli/src/conf/routes.php
+++ Projet/src/gift.appli/src/conf/routes.php
@@ -4,7 +4,7 @@ use Slim\Psr7\Request;
 use Slim\Psr7\Response;
 
 return function (App $app) {
-    $app->get('/', function (Request $request, Response $response, array $args) {
+    $app->get('/Projet/Projet/src/gift.appli/public/index.php', function (Request $request, Response $response, array $args) {
         $response->getBody()->write("Hello world!");
         return $response;
     });
