35cfb6522b7bed0527ec52e9f9430213462310d8
Dami1 <damien.melchior3@etu.univ-lorraine.fr>
2023-05-11 09:21:02 +0200

    TD2 EX2 Q1

--- gift/gift.appli/src/conf/routes.php
+++ gift/gift.appli/src/conf/routes.php
@@ -8,4 +8,26 @@ return function (App $app) {
         $response->getBody()->write("Hello world!");
         return $response;
     });
+
+    /**
+     * Question 1
+     */
+    $app->get('/categories[/]', function (Request $request, Response $response, array $args) {
+        $html = <<<HTML
+        <html>
+        <head>
+        <title>Categories</title>
+        </head>
+        <body>
+        <h1>Categories</h1>
+        <ul>
+            <li>1. <a> href="/categories/1">restauration</a></li>
+            <li>2. <a> href="/categories/2">hébergement</a></li>
+            <li>3. <a> href="/categories/3">attention</a></li>
+            <li>4. <a> href="/categories/3">activités</a></li>
+        </ul></body></html>
+HTML;
+        $response->getBody()->write($html);
+        return $response;
+    });
 };
