243ce6ba1d36793d7b159b76c047e1f9969eeec3
Stanislas JEANGEY <jeangey1u@etu.univ-lorraine.fr>
2023-05-15 17:12:05 +0200

    petit oublie dans les liens de categories

--- gift/gift.appli/src/actions/GetCategorieAction.php
+++ gift/gift.appli/src/actions/GetCategorieAction.php
@@ -18,10 +18,10 @@ class GetCategorieAction extends AbstractAction
         <body>
         <h1>Categories</h1>
         <ul>
-            <li>1. <a href="/gift.appli/public/categories/1">restauration</a></li>
-            <li>2. <a href="/gift.appli/public/categories/2">hébergement</a></li>
-            <li>3. <a href="/gift.appli/public/categories/3">attention</a></li>
-            <li>4. <a href="/gift.appli/public/categories/4">activités</a></li>
+            <li>1. <a href="/categories/1">restauration</a></li>
+            <li>2. <a href="/categories/2">hébergement</a></li>
+            <li>3. <a href="/categories/3">attention</a></li>
+            <li>4. <a href="/categories/4">activités</a></li>
         </ul></body></html>
 HTML;
         $response->getBody()->write($html);
