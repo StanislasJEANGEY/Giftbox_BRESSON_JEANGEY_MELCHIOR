4252f7efb2996400f58bb547c2eb4171204ced49
Stanislas JEANGEY <jeangey1u@etu.univ-lorraine.fr>
2023-05-16 10:37:21 +0200

    prestation dans les catégories zebi

--- gift/gift.appli/src/actions/GetCategorieByIdAction.php
+++ gift/gift.appli/src/actions/GetCategorieByIdAction.php
@@ -10,19 +10,36 @@ class GetCategorieByIdAction extends AbstractAction
     public function __invoke(ServerRequestInterface $request, ResponseInterface $response, array $args) : ResponseInterface
     {
         $id = $args['id'];
-        $cat = CATEGS[$id];
-
+        $categorie = \gift\app\models\Categorie::find($id);
         $html = <<<HTML
-    <html>
-    <head>
-    <title>Categorie $id</title>
-    </head>
-    <body>
-    <h1>La Categorie $id</h1>
-    <h2>{$cat['libelle']}</h2>
-    <h2>{$cat['description']}</h2>
-    </body></html>
-HTML;
+        <html>
+            <head>
+                <title>$categorie->libelle</title>
+            </head>
+            <body>
+                <h1>$categorie->libelle</h1>
+        HTML;
+
+        $prestations = \gift\app\models\Prestation::where('cat_id', '=', $id)->get();
+        $html .= '<ul>';
+        foreach ($prestations as $presta) {
+            $html .= <<<HTML
+                <li> $presta->libelle : $presta->tarif €
+                <br>
+                Description : $presta->description
+                <br>
+                <img src=$presta->img alt="image de $presta->img">
+                </li>
+                <br>
+            HTML;
+        }
+        $html .= '</ul>';
+        $html .= <<<HTML
+            </body>
+        </html>
+        HTML;
+
+
         $response->getBody()->write($html);
         return $response;
     }
