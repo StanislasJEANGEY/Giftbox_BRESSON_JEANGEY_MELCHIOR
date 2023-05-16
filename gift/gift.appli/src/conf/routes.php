57917c0c94f383440f16a0fbcf7ec20f21580dfc
Stanislas JEANGEY <jeangey1u@etu.univ-lorraine.fr>
2023-05-16 10:23:24 +0200

    delete some informations not utile

--- gift/gift.appli/src/conf/routes.php
+++ gift/gift.appli/src/conf/routes.php
@@ -3,19 +3,6 @@ use Slim\App;
 use Slim\Psr7\Request;
 use Slim\Psr7\Response;
 
-define("CATEGS", [
-    1=>['libelle'=>'restauration', 'description'=>'les restos quoua'],
-    2=>['libelle'=>'hébergement', 'description'=>'les hotels, chambre quoua'],
-    3=>['libelle'=>'attention', 'description'=>'les trucs qui font plaizzz'],
-    4=>['libelle'=>'activités', 'description'=>'les trucs à faire'],
-]);
-
-define ("PRESTA", [
-    "ABCD"=>['libelle'=>'diner', 'description'=>'diner de gala', 'tarif'=>100, 'unite'=>'par personne'],
-    "EFGH"=>['libelle'=>'déjeuner', 'description'=>'déjeuner de gala', 'tarif'=>50, 'unite'=>'par personne'],
-    "IJKL"=>['libelle'=>'saut parachute', 'description'=>'un saut en parachute filmé', 'tarif'=>400, 'unite'=>'par personne'],
-    "MNOP"=>['libelle'=>'massage', 'description'=>'un massage de 1h', 'tarif'=>100, 'unite'=>'par personne'],
-]);
 
 return function (App $app) {
     $app->get('[/]', function (Request $request, Response $response, array $args) {
