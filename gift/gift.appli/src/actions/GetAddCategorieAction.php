deb8e7b76c4068d6fea80d3b0bd5f84415aa1981
BJulOs <julesbresson88600@gmail.com>
2023-05-25 09:43:20 +0200

    Exo 2 TD6 fonctionnel

--- gift/gift.appli/src/actions/GetAddCategorieAction.php
+++ gift/gift.appli/src/actions/GetAddCategorieAction.php
@@ -3,21 +3,30 @@
 namespace gift\app\actions;
 
 use gift\app\services\prestations\PrestationsService;
+use gift\app\services\prestations\PrestationsServiceException;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 use Slim\Views\Twig;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Error\SyntaxError;
 
 class GetAddCategorieAction extends AbstractAction
 {
 
-    public function __invoke(ServerRequestInterface $request, ResponseInterface $response, array $args): ResponseInterface
+	/**
+	 * @throws SyntaxError
+	 * @throws PrestationsServiceException
+	 * @throws RuntimeError
+	 * @throws LoaderError
+	 */
+	public function __invoke(ServerRequestInterface $request, ResponseInterface $response, array $args): ResponseInterface
     {
         $prestaService = new PrestationsService();
         if ($request->getMethod() === 'POST') {
-            $data = $request->getParsedBody();
-            $prestaService->getCreateCategorie($data['libelle'], $data['description']);
-            header('Location: /categories');
-			exit();
+			$attributs = $request->getParsedBody();
+			$prestaService->getCreateCategorie($attributs);
+			return $response->withHeader('Location', '/categories')->withStatus(302);
         }
         $view = Twig::fromRequest($request);
         return $view->render($response, 'AddCategorieView.twig');
